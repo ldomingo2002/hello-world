0ffebdcb765d6a685e247db9ac0b8198cebe514e
Leslie Dominguez <LeslieDominguez601@Gmail.com>
2018-11-06 13:01:08 -0500

    Added more javascript and deleted inital hello world

--- function.js
+++ function.js
@@ -8,11 +8,11 @@ alert('Hello, World!');
 }
 
 function helloBrowser (){
-  var p = document.getElementById('browser');
+  var p=document.getElementById('browser');
   p.innerHTML = "Hello, World!";
 }
 
 function helloStranger (){
 prompt('Enter your name');
-
+console.log();
 }
