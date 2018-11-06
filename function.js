605c76a8fe5a77ef19e1be893537991e65ec5bec
Leslie Dominguez <LeslieDominguez601@Gmail.com>
2018-11-06 12:45:48 -0500

    Added buttons to page as well as some Javascript on a separate file

---
+++ function.js
@@ -0,0 +1,18 @@
+function helloConsole (){
+  console.log('Hello, World!');
+}
+
+function helloPopUp (){
+
+alert('Hello, World!');
+}
+
+function helloBrowser (){
+  var p = document.getElementById('browser');
+  p.innerHTML = "Hello, World!";
+}
+
+function helloStranger (){
+prompt('Enter your name');
+
+}
